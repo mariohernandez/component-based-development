5acfbfb92fa6360398c72ae6e76dbefd95ff76fd
Mario Hernandez <mario.hernandez@mediacurrent.com>
2018-10-14 17:41:11 -0700

    Renames card to movie-card

--- web/themes/custom/nitflex_theme/src/components/movie-card/movie-card.es6.js
+++ web/themes/custom/nitflex_theme/src/components/movie-card/movie-card.es6.js
@@ -1,36 +1,34 @@
-!function (document, Drupal, $) {
+!((document, Drupal, $) => {
   'use strict';
 
   /**
    * Creates behavior for Add to Watch list.
    */
-
   Drupal.behaviors.addToWatchList = {
-    attach: function attach(context) {
+    attach: function(context) {
       /**
        * Bind open/close action to list containers.
-       * @param {string} container - Card conteainer.
+       * @param {string} container - Movie card conteainer.
        * @param {string} button - Trigger button.
        * @param {context} context - Drupal context.
        * @returns {void}
        */
       function addToList(container, button, context) {
-        var $container = $(container, context);
+        const $container = $(container, context);
 
-        $container.each(function () {
-          var $cardItem = $(this);
-          $cardItem.find(button).on('click', function () {
+        $container.each(function() {
+          const $movieCardItem = $(this);
+          $movieCardItem.find(button).on('click', function() {
             // Adds the 'is-added' class to cards where
             // movie has been added to watch list.
-            $container.not($cardItem).removeClass('is-added');
-            $cardItem.toggleClass('is-added');
+            $container.not($movieCardItem).removeClass('is-added');
+            $movieCardItem.toggleClass('is-added');
           });
         });
       }
 
-      // Bind the function to the card.
+      // Bind the function to the movie card.
       addToList('.card', '.js-add-to-list', context);
     }
   };
-}(document, Drupal, jQuery);
-//# sourceMappingURL=card.js.map
+})(document, Drupal, jQuery);
