be635de97b354651df25bfb80b02a3319e2399a1
Melissa Bent <melissa.bent@mediacurrent.com>
2019-08-26 15:44:04 -0600

    Add lando script for first time build
    Added a new script file to handle basic build processes. This
    should ensure that the steps are only run the first time the
    lando environment is setup and should reduce import time.

---
+++ scripts/init.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+if [ ! -z $LANDO_MOUNT ]; then
+  cd $LANDO_MOUNT
+
+  # Copy asset files.
+  if [ ! -d "web/sites/default/files" ]; then
+    echo "Creating Drupal default files directory"
+    chmod 755 web/sites/default
+    mkdir web/sites/default/files
+    chmod 777 web/sites/default/files
+    chmod 555 web/sites/default
+  fi
+  cp -r assets/imgs/. web/sites/default/files/.
+
+  # Check if the DB is empty before importing the gzipped backup.
+  DRUPAL_NOT_INSTALLED=`drush status bootstrap | grep -q Successful`
+  if [ DRUPAL_NOT_INSTALLED ]; then
+    echo "Importing Drupal database"
+    gunzip -c drupal8.export.gz | drush sqlc
+  fi
+fi
